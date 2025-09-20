7de4283e098ca96c4e5abe4c087aa7ba5bc36ecc
dev-tsampaio <thaleslopessampaio.ti@hotmail.com>
2025-09-20 09:05:53 -0300

    feat: Exemplo usando estrutra for.

---
+++ src/usandoFor.java
@@ -0,0 +1,12 @@
+public class usandoFor {
+    public static void main(String[] args) {
+        //Imprimindo todos os numeros pares de 0 até 16
+        // for = "repita isso x vezes.
+
+        for(int i = 0; i <= 16; i++){ // Repita para os numeros de 0 a 16
+            if (i % 2 == 0){ // Se o resto da divisão de i = 0 , ele é par!
+                System.out.println(i); // Imprima I, que serão os numeros pares.
+            }
+        }
+    }
+}
